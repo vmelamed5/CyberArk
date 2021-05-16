eac9a07fd3061167221d33dcc99a8bb61e427acd
vmelamed5 <vmelamed5@gmail.com>
2021-05-16 19:31:36 -0400

    Update VDeleteApplicationAuthentication.ps1

--- VpasModule/Public/VDeleteApplicationAuthentication.ps1
+++ VpasModule/Public/VDeleteApplicationAuthentication.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .Synopsis
    DELETE APPLICATION ID AUTHENTICATION METHOD
    CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
@@ -32,7 +32,13 @@ function VDeleteApplicationAuthentication{
         [String]$AuthType,
 
         [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=4)]
-        [String]$AuthValue
+        [String]$AuthValue,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=5)]
+        [Switch]$NoSSL,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=6)]
+        [String]$AuthID
     
     )
 
@@ -42,27 +48,65 @@ function VDeleteApplicationAuthentication{
     Write-Verbose "AUTHTYPE VALUE SET: $AuthType"
     Write-Verbose "AUTHVALUE VALUE SET: $AuthValue"
 
-    Write-Verbose "CALLING HELPER FUNCTION TO RETRIEVE AUTH ID"
-    $AuthID = VGetApplicationAuthIDHelper -PVWA $PVWA -token $token -AppID $AppID -AuthType $AuthType -AuthValue $AuthValue
-    Write-Verbose "HEPER FUNCTION RETURNED VALUE"
+    if([String]::IsNullOrEmpty($AuthID)){
 
-    if($AuthID -eq -1){
-        Write-Verbose "COULD NOT FIND TARGET AUTHENTICATION METHOD TO DELETE, CONFIRM $AppID, $AuthType, $AuthValue EXISTS"
-        Vout -str "COULD NOT FIND TARGET AUTHENTICATION METHOD TO DELETE, CONFIRM $AppID, $AuthType, $AuthValue EXISTS" -type E
-        return $false
-    }
-    else{
-        try{
-            write-verbose "FOUND UNIQUE AUTHID"
-            $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications/$AuthID"
-            $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method DELETE
-            #Vout -str $response -type C
-            Write-Verbose "AUTHID VALUE WAS DELETED SUCCESSFULLY"
-            return $true
-        }catch{
-            Vout -str $_ -type E
-            Write-Verbose "FAILED TO DELETE AUTHID VALUE"
+        Write-Verbose "NO AUTH ID PROVIDED, INVOKING HELPER FUNCTION"
+    
+        if($NoSSL){
+            $AuthID = VGetApplicationAuthIDHelper -PVWA $PVWA -token $token -AppID $AppID -AuthType $AuthType -AuthValue $AuthValue -NoSSL
+        }
+        else{
+            $AuthID = VGetApplicationAuthIDHelper -PVWA $PVWA -token $token -AppID $AppID -AuthType $AuthType -AuthValue $AuthValue
+        }
+        Write-Verbose "HEPER FUNCTION RETURNED VALUE"
+
+        if($AuthID -eq -1){
+            Write-Verbose "COULD NOT FIND TARGET AUTHENTICATION METHOD TO DELETE, CONFIRM $AppID, $AuthType, $AuthValue EXISTS"
+            Vout -str "COULD NOT FIND TARGET AUTHENTICATION METHOD TO DELETE, CONFIRM $AppID, $AuthType, $AuthValue EXISTS" -type E
             return $false
         }
+        else{
+            try{
+                write-verbose "FOUND UNIQUE AUTHID"
+            
+                if($NoSSL){
+                    Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                    $uri = "http://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications/$AuthID"
+                }
+                else{
+                    Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                    $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications/$AuthID"
+                }
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method DELETE
+                #Vout -str $response -type C
+                Write-Verbose "AUTHID VALUE WAS DELETED SUCCESSFULLY"
+                return $true
+            }catch{
+                Vout -str $_ -type E
+                Write-Verbose "FAILED TO DELETE AUTHID VALUE"
+                return $false
+            }
+        }
+    }
+    else{
+        Write-Verbose "AUTH ID PROVIDED, SKIPPING HELPER FUNCTION"
+            try{            
+                if($NoSSL){
+                    Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                    $uri = "http://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications/$AuthID"
+                }
+                else{
+                    Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                    $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications/$AuthID"
+                }
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method DELETE
+                #Vout -str $response -type C
+                Write-Verbose "AUTHID VALUE WAS DELETED SUCCESSFULLY"
+                return $true
+            }catch{
+                Vout -str $_ -type E
+                Write-Verbose "FAILED TO DELETE AUTHID VALUE"
+                return $false
+            }
     }
 }
