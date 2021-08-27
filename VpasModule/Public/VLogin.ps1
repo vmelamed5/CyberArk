93540463f3ad79aabc1002d9b8e37c351465f428
vmelamed5 <vmelamed5@gmail.com>
2021-08-26 21:57:13 -0400

    Update VLogin.ps1

--- VpasModule/Public/VLogin.ps1
+++ VpasModule/Public/VLogin.ps1
@@ -7,7 +7,7 @@
 .EXAMPLE
    $token = VLogin -PVWA {PVWA VALUE} -AuthType radius
 .EXAMPLE
-   $token = VLogin -PVWA {PVWA VALUE} -AuthType cyberark -creds {PSCredential VALUE}
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType cyberark 
 #>
 function VLogin{
     [CmdletBinding()]
@@ -16,15 +16,11 @@ function VLogin{
         [String]$PVWA,
 
         [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
-        [ValidateSet('cyberark','radius','saml')]
+        [ValidateSet('cyberark','radius')]
         [String]$AuthType,
-        
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
-        [PSCredential]$creds,
 
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
-        [Switch]$NoSSL
-    
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [PSCredential]$creds
     )
     
     Vout -str " __      _______          __  __           _       _       " -type G
@@ -36,69 +32,36 @@ function VLogin{
     
     if($AuthType -eq "radius"){
         Write-Verbose "RADIUS AUTHENTICATION SELECTED"
-        
-        if($NoSSL){
-            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
-            $uri = "http://$PVWA/PasswordVault/API/auth/RADIUS/Logon"
-        }
-        else{
-            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
-            $uri = "https://$PVWA/PasswordVault/API/auth/RADIUS/Logon"
-        }
-    }
-    if($AuthType -eq "saml"){
-        Write-Verbose "SAML AUTHENTICATION SELECTED"
-        
-        if($NoSSL){
-            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
-            $uri = "http://$PVWA/PasswordVault/API/auth/SAML/Logon"
-        }
-        else{
-            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
-            $uri = "https://$PVWA/PasswordVault/API/auth/SAML/Logon"
-        }
+        $uri = "https://$PVWA/PasswordVault/API/auth/RADIUS/Logon"
     }
     if($AuthType -eq "cyberark"){
         Write-Verbose "CYBERARK AUTHENTICATION SELECTED"
-
-        if($NoSSL){
-            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
-            $uri = "http://$PVWA/PasswordVault/API/auth/cyberark/Logon"
-        }
-        else{
-            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
-            $uri = "https://$PVWA/PasswordVault/API/auth/cyberark/Logon"
-        }
+        $uri = "https://$PVWA/PasswordVault/API/auth/cyberark/Logon"
     }
 
     if(!$creds){
-         $creds = Get-Credential -Message 'ENTER CYBERARK CREDENTIALS'
+        $creds = Get-Credential -Message 'ENTER CYBERARK CREDENTIALS'
     }
     $username = $creds.GetNetworkCredential().UserName
     $password = $creds.GetNetworkCredential().Password
     Write-Verbose "CYBERARK CREDENTIALS SET"
-
+    
     #GET LOGIN TOKEN
     try{
+        #GENERATE SESSION NUMBER
+        $SessionNumber = Get-Random -Minimum 0 -Maximum 100
+        Write-Verbose "SESSION NUMBER SET TO $SessionNumber"
+
         $params = @{
             username = $username;
             password = $password;
-        }
-        
-        if(([Net.SecurityProtocolType].GetEnumNames() -contains "Tls12") -and (-not ([System.Net.ServicePointManager]::SecurityProtocol -match "Tls12"))){
+            concurrentSession = $SessionNumber;
+        } | ConvertTo-Json	
+        Write-Verbose "API PARAMETERS SET"
+		if(([Net.SecurityProtocolType].GetEnumNames() -contains "Tls12" ) -and (-not ([System.Net.ServicePointManager]::SecurityProtocol -match "Tls12"))){
             [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         }
-        
-        if($response = Invoke-WebRequest -Uri $uri -Method POST -Body $params){
-            Write-Verbose "TRIMMING LOGIN TOKEN VALUE"
-            $token = $response.Content
-            $token = $token.Replace('"',"")
-            Write-Verbose "LOGIN TOKEN RETRIEVED"
-        }
-        else{
-            Vout -str "UNKOWN ERROR, CONFIRM CORRECT PVWA, USERNAME, AND PASSWORD WAS SUPPLIED" -type E
-            return $false
-        }
+        $token = Invoke-RestMethod -Uri $uri -Method Post -Body $params -ContentType 'application/json'
     }catch{
         Vout -str $_ -type E
         return $false
@@ -106,5 +69,3 @@ function VLogin{
     Write-Verbose "RETURNING LOGIN TOKEN"
     return $token
 }
-
-
