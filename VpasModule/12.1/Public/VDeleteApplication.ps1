7e424d91b16b6bb4261f4c6d2f15dd88965f6b17
vmelamed5 <vmelamed5@gmail.com>
2021-09-11 21:35:06 -0400

    Add files via upload

---
+++ VpasModule/12.1/Public/VDeleteApplication.ps1
@@ -0,0 +1,51 @@
+<#
+.Synopsis
+   DELETE APPLICATION ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   THIS FUNCTION DELETES AN APPLICATION ID FROM CYBERARK
+.EXAMPLE
+   VDeleteApplication -PVWA {PVWA VALUE} -token {TOKEN VALUE} -AppID {APPLICATION ID VALUE}
+#>
+function VDeleteApplication{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$AppID,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "PVWA VALUE SET: $PVWA"
+    Write-Verbose "TOKEN VALUE SET: $token"
+    Write-Verbose "APPID VALUE SET: $AppID"
+
+    try{
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/"
+        }
+        $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method DELETE
+        Write-Verbose "$AppID DELETED FROM CYBERARK"
+        $output = $response
+    }catch{
+        Write-Verbose "FAILED TO DELETE $AppID, CONFIRM APPID EXISTS IN CYBERARK"
+        Vout -str $_ -type E
+        $output = $false
+    }
+
+    return $output
+}
---
+++ VpasModule/12.1/Public/VGetSafes.ps1
@@ -0,0 +1,51 @@
+<#
+.Synopsis
+   GET CYBERARK SAFES
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO RETRIEVE SAFES BASED ON A SEARCH QUERY
+.EXAMPLE
+   $out = VGetSafes -PVWA {PVWA VALUE} -token {TOKEN VALUE} -searchQuery {SEARCHQUERY VALUE}
+#>
+function VGetSafes{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$searchQuery,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED SEARCHQUERY VALUE"
+
+    try{
+        write-verbose "MAKING API CALL TO CYBERARK"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/WebServices/PIMServices.svc/Safes?query=$searchQuery"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Safes?query=$searchQuery"
+        }
+        $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method GET
+        Write-Verbose "PARSING DATA FROM CYBERARK"
+        Write-Verbose "RETURNING ARRAY OF SAFE VALUES"
+        return $response
+    }catch{
+        Write-Verbose "FAILED TO RETRIEVE SAFES"
+        Vout -str $_ -type E
+        return $false
+    }
+}
