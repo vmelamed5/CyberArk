79b0a38b0f895820d2af2ff7d3b4a4f94a5a7ce5
vmelamed5 <vmelamed5@gmail.com>
2022-02-08 16:50:27 -0500

    Update VAddAllowedReferrer.ps1

--- VpasModule/12.2.1/public/VAddAllowedReferrer.ps1
+++ VpasModule/12.2.1/public/VAddAllowedReferrer.ps1
@@ -68,10 +68,10 @@ function VAddAllowedReferrer{
         write-verbose "MAKING API CALL TO CYBERARK"
 
         if($sessionval){
-            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method POST -ContentType "application/json" -WebSession $sessionval
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method POST -Body $params -ContentType "application/json" -WebSession $sessionval
         }
         else{
-            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method POST -ContentType "application/json"  
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method POST -Body $params -ContentType "application/json"  
         }
         Write-Verbose "SUCCESSFULLY ADDED ALLOWED REFERRER: $ReferrerURL"
         return $true
