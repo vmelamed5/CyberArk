6d7d5cd387f1f6fd2334125e121588629ca3acf6
vmelamed5 <vmelamed5@gmail.com>
2022-02-12 15:16:32 -0500

    Add files via upload

---
+++ VpasModule/12.2/public/VGetDiscoveredAccounts.ps1
@@ -0,0 +1,80 @@
+<#
+.Synopsis
+   GET DISCOVERED ACCOUNTS
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO GET DISCOVERED ACCOUNTS IN THE PENDING SAFE LIST
+.EXAMPLE
+   $DiscoveredAccountsJSON = VGetDiscoveredAccounts -PVWA {PVWA VALUE} -token {TOKEN VALUE} -SearchQuery {SEARCHQUERY VALUE}
+.OUTPUTS
+   JSON Object (DiscoveredAccounts) if successful
+   $false if failed
+#>
+function VGetDiscoveredAccounts{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$SearchQuery,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [String]$limit,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [String]$offset,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=5)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED SEARCHQUERY VALUE: $SearchQuery"
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/passwordvault/api/DiscoveredAccounts?search=$SearchQuery"
+            if(![String]::IsNullOrEmpty($limit)){
+                $uri += "&limit=$limit"
+            }
+            if(![String]::IsNullOrEmpty($offset)){
+                $uri += "&offset=$offset"
+            }
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/passwordvault/api/DiscoveredAccounts?search=$SearchQuery"
+            if(![String]::IsNullOrEmpty($limit)){
+                $uri += "&limit=$limit"
+            }
+            if(![String]::IsNullOrEmpty($offset)){
+                $uri += "&offset=$offset"
+            }
+        }
+
+        write-verbose "MAKING API CALL TO CYBERARK"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json"  
+        }
+        Write-Verbose "RETURNING JSON OBJECT"
+        return $response
+    }catch{
+        Write-Verbose "UNABLE TO GET DISCOVERED ACCOUNTS FOR SEARCHQUERY: $SearchQuery"
+        Vout -str $_ -type E
+        return $false
+    }
+}
---
+++ VpasModule/12.2/public/VGetSafes.ps1
@@ -0,0 +1,81 @@
+<#
+.Synopsis
+   GET CYBERARK SAFES
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO RETRIEVE SAFES BASED ON A SEARCH QUERY
+.EXAMPLE
+   $SafesJSON = VGetSafes -PVWA {PVWA VALUE} -token {TOKEN VALUE} -searchQuery {SEARCHQUERY VALUE}
+.OUTPUTS
+   JSON Object (Safes) if successful
+   $false if failed
+#>
+function VGetSafes{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$searchQuery,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [String]$limit,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [String]$offset,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=5)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED SEARCHQUERY VALUE"
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+
+        write-verbose "MAKING API CALL TO CYBERARK"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/api/safes?search=$searchQuery"
+            if(![String]::IsNullOrEmpty($limit)){
+                $uri += "&limit=$limit"
+            }
+            if(![String]::IsNullOrEmpty($offset)){
+                $uri += "&offset=$offset"
+            }
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/api/safes?search=$searchQuery"
+            if(![String]::IsNullOrEmpty($limit)){
+                $uri += "&limit=$limit"
+            }
+            if(![String]::IsNullOrEmpty($offset)){
+                $uri += "&offset=$offset"
+            }
+        }
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json"  
+        }
+        Write-Verbose "PARSING DATA FROM CYBERARK"
+        Write-Verbose "RETURNING JSON OBJECT"
+        return $response
+    }catch{
+        Write-Verbose "FAILED TO RETRIEVE SAFES"
+        Vout -str $_ -type E
+        return $false
+    }
+}
