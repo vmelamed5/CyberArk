6d31c4585ccfd65abfed89b894b322df05f22941
vmelamed5 <vmelamed5@gmail.com>
2022-12-22 11:48:35 -0500

    Add files via upload

---
+++ VpasModule/12.6.1/public/VGetUsagePlatformDetails.ps1
@@ -0,0 +1,79 @@
+<#
+.Synopsis
+   GET USAGE PLATFORM DETAILS
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO GET USAGE PLATFORM DETAILS
+.EXAMPLE
+   $UsagePlatformDetailsJSON = VGetUsagePlatformDetails -token {TOKEN VALUE} -usageplatformID {USAGE PLATFORMID VALUE}
+.OUTPUTS
+   JSON Object (UsagePlatformDetails) if successful
+   $false if failed
+#>
+function VGetUsagePlatformDetails{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$usageplatformID,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [Switch]$NoSSL
+    
+    )
+
+    $platformID = $usageplatformID
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+
+        Write-Verbose "CONSTRUCTING SEARCH STRING TO QUERY CYBERARK"
+        $searchQuery = "$platformID"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/passwordvault/api/platforms/dependents"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/passwordvault/api/platforms/dependents"
+        }
+        write-verbose "MAKING API CALL"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json"  
+        }
+
+
+        $counter = $response.Total
+        Write-Verbose "FOUND $counter USAGE PLATFORMS...LOOKING FOR TARGET USAGE PLATFORMID: $searchQuery"
+
+        $output = -1
+        foreach($rec in $response.Platforms){
+            $recid = $rec.ID
+            $recplatformid = $rec.PlatformID
+            $recname = $rec.Name
+
+            if($recplatformid -eq $platformID -or $recname -eq $platformID){
+                $output = $rec
+                Write-Verbose "FOUND $platformID : TARGET ENTRY FOUND, RETURNING DETAILS"
+                return $output
+            }
+            Write-Verbose "FOUND $recplatformid : NOT TARGET ENTRY (SKIPPING)"
+
+        }
+        Write-Verbose "CAN NOT FIND TARGET ENTRY, RETURNING -1"
+        Vout -str "UNABLE TO FIND TARGET USAGE PLATFORMID, RETURNING -1" -type E
+        return $output
+    }catch{
+        Write-Verbose "UNABLE TO QUERY CYBERARK"
+        Vout -str $_ -type E
+    }
+}
---
+++ VpasModule/12.6.1/public/VLogin.ps1
@@ -0,0 +1,258 @@
+<#
+.Synopsis
+   GET CYBERARK LOGIN TOKEN
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO AUTHENTICATE INTO CYBERARK VIA RADIUS, CYBERARK, WINDOWS, SAML, OR LDAP AUTH
+.EXAMPLE
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType radius
+.EXAMPLE
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType cyberark
+.EXAMPLE
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType windows 
+.EXAMPLE
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType ldap 
+.EXAMPLE
+   $token = VLogin -PVWA {PVWA VALUE} -AuthType saml 
+.OUTPUTS
+   Cyberark Login Token if successful
+   $false if failed
+#>
+function VLogin{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [ValidateSet('cyberark','radius','windows','ldap','saml')]
+        [String]$AuthType,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [PSCredential]$creds,
+	
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$HideAscii,
+	
+	    [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [Switch]$NoSSL,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=5)]
+        [Switch]$InitiateCookie,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=6)]
+        [String]$IDPLogin
+    )
+    
+    $output = @{}
+
+    if($HideAscii){
+    	#DO NOTHING
+    }
+    else{
+        Vout -str " __      _______          __  __           _       _       " -type G
+        Vout -str " \ \    / /  __ \        |  \/  |         | |     | |      " -type G
+        Vout -str "  \ \  / /| |__) |_ _ ___| \  / | ___   __| |_   _| | ___  " -type G
+        Vout -str "   \ \/ / |  ___/ _`  / __| |\/| |/ _ \ / _`  | | | | |/ _ \ " -type G
+        Vout -str "    \  /  | |  | (_| \__ \ |  | | (_) | (_| | |_| | |  __/ " -type G
+        Vout -str "     \/   |_|   \__,_|___/_|  |_|\___/ \__,_|\__,_|_|\___| " -type G  
+    }
+    
+    if(([Net.SecurityProtocolType].GetEnumNames() -contains "Tls12" ) -and (-not ([System.Net.ServicePointManager]::SecurityProtocol -match "Tls12"))){
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    }
+
+    if($AuthType -ne "saml"){
+        if($AuthType -eq "radius"){
+            Write-Verbose "RADIUS AUTHENTICATION SELECTED"
+	
+	        if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/auth/RADIUS/Logon"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/auth/RADIUS/Logon"
+            }
+	
+        }
+        if($AuthType -eq "cyberark"){
+            Write-Verbose "CYBERARK AUTHENTICATION SELECTED"
+
+	        if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/auth/cyberark/Logon"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/auth/cyberark/Logon"
+            }
+        }
+        if($AuthType -eq "windows"){
+            Write-Verbose "WINDOWS AUTHENTICATION SELECTED"
+
+	        if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/auth/Windows/Logon"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/auth/Windows/Logon"
+            }
+        }
+        if($AuthType -eq "ldap"){
+            Write-Verbose "LDAP AUTHENTICATION SELECTED"
+
+	        if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/auth/LDAP/Logon"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/auth/LDAP/Logon"
+            }
+        }
+    
+        if(!$creds){
+            $creds = Get-Credential -Message 'ENTER CYBERARK CREDENTIALS'
+        }
+        $username = $creds.GetNetworkCredential().UserName
+        $password = $creds.GetNetworkCredential().Password
+        Write-Verbose "CYBERARK CREDENTIALS SET"
+    
+        #GET LOGIN TOKEN
+        try{
+            $params = @{
+                username = $username;
+                password = $password;
+                concurrentSession = $true;
+            } | ConvertTo-Json	
+            Write-Verbose "API PARAMETERS SET"
+		
+
+            if($InitiateCookie){
+                Write-Verbose "INITIATING COOKIE"
+                $cookie = new-object system.net.cookie
+                $cookie.name = "tos_accepted"
+                $cookie.domain = "$PVWA"
+                $session = new-object microsoft.powershell.commands.webrequestsession
+                $session.cookies.add($cookie)
+
+                $token = Invoke-RestMethod -Uri $uri -Method Post -Body $params -ContentType 'application/json' -WebSession $session
+                Write-Verbose "RETURNING LOGIN TOKEN AND COOKIE SESSION"
+                $output = @{
+                    token = $token
+                    session = $session
+                    pvwa = $PVWA
+                }
+                return $output
+            }
+            else{
+                $token = Invoke-RestMethod -Uri $uri -Method Post -Body $params -ContentType 'application/json'
+                Write-Verbose "RETURNING LOGIN TOKEN"
+                $output = @{
+                    token = $token
+                    pvwa = $PVWA
+                }
+                return $output
+            }
+        }catch{
+            Vout -str $_ -type E
+            return $false
+        }
+    }
+    elseif($AuthType -eq "saml"){
+        if([String]::IsNullOrEmpty($IDPLogin)){
+            write-host "SAML SELECTED BUT NO IDPLogin PROVIDED, PLEASE ENTER IDPLogin URL (Example: https://auth.vman.com/app/vman_cyberark/lkadjlk67843HJdkJ/sso/saml: " -ForegroundColor Yellow -NoNewline
+            $IDPLogin = Read-host
+        }
+        try{
+            $targetExp = '(?i)name="SAMLResponse"(?: type="hidden")? value=\"(.*?)\"(?:.*)?\/>' 
+            Add-Type -AssemblyName System.Windows.Forms 
+            Add-Type -AssemblyName System.Web
+
+            $LoginForm = New-Object Windows.Forms.Form
+            $LoginForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
+            $LoginForm.Width = 640
+            $LoginForm.Height = 700
+            $LoginForm.showIcon = $false
+            $LoginForm.Topmost = $true
+
+            $InitializeWeb = New-Object Windows.Forms.WebBrowser
+            $InitializeWeb.Size = $LoginForm.ClientSize
+            $InitializeWeb.Anchor = "Left,Top,Right,Bottom"
+            $InitializeWeb.ScriptErrorsSuppressed = $true
+ 
+            $LoginForm.Controls.Add($InitializeWeb)
+ 
+            $InitializeWeb.Navigate($IDPLogin)
+            $InitializeWeb.add_Navigating({
+                if($InitializeWeb.DocumentText -match "SAMLResponse"){
+                    $_.cancel = $true
+ 
+                    if($InitializeWeb.DocumentText -match $targetExp){
+                        $LoginForm.Close()
+                        $SAMLToken = $(($Matches[1] -replace '&#x2b;', '+') -replace '&#x3d;', '=')
+                    }
+                }
+            })
+
+            if($LoginForm.ShowDialog() -ne "OK"){
+                if($null -ne $SAMLToken){
+                    $LoginForm.Close()
+                    Remove-Variable -Name SAMLToken -Scope Script -ErrorAction SilentlyContinue
+                }
+                else{
+                    throw "SAMLResponse not matched"
+                }
+            }
+            $LoginForm.Dispose()
+
+            if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/auth/SAML/Logon"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/auth/SAML/Logon"
+            }
+
+            $params = @{
+                concurrentSession='true'
+                apiUse='true'
+                SAMLResponse=$SamlToken
+            }
+            Write-Verbose "API PARAMETERS SET"
+
+            if($InitiateCookie){
+                Write-Verbose "INITIATING COOKIE"
+                $cookie = new-object system.net.cookie
+                $cookie.name = "tos_accepted"
+                $cookie.domain = "$PVWA"
+                $session = new-object microsoft.powershell.commands.webrequestsession
+                $session.cookies.add($cookie)
+
+                $token = Invoke-RestMethod -Uri $uri -Method Post -body $params -ContentType 'application/x-www-form-urlencoded'
+                Write-Verbose "RETURNING LOGIN TOKEN AND COOKIE SESSION"
+                $output = @{
+                    token = $token
+                    session = $session
+                    pvwa = $PVWA
+                }
+                return $output
+            }
+            else{
+                $token = Invoke-RestMethod -Uri $uri -Method Post -body $params -ContentType 'application/x-www-form-urlencoded'
+                Write-Verbose "RETURNING LOGIN TOKEN"
+                $output = @{
+                    token = $token
+                    pvwa = $PVWA
+                }
+                return $output
+            }
+        }catch{
+            Vout -str $_ -type E
+            return $false
+        }
+    }
+}
