e463e20adf7d5abe8dd883b0138cf408a5e35dcd
vmelamed5 <vmelamed5@gmail.com>
2023-06-28 10:57:48 -0400

    Add files via upload

---
+++ VpasModule/13.1.1/private/Get-VPASPlatformIDHelper.ps1
@@ -0,0 +1,76 @@
+<#
+.Synopsis
+   GET PLATFORM ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   HELPER FUNCTION TO RETRIEVE PLATFORM IDS FROM CYBERARK
+#>
+function Get-VPASPlatformIDHelper{
+    [OutputType([String],'System.Int32')]
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$platformID,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [Switch]$NoSSL
+
+    )
+
+    Begin{
+        $tokenval,$sessionval,$PVWA,$Header,$ISPSS,$IdentityURL = Get-VPASSession -token $token
+    }
+    Process{
+        try{
+            Write-Verbose "CONSTRUCTING SEARCH STRING TO QUERY CYBERARK"
+            $searchQuery = "$platformID"
+
+            if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/passwordvault/api/platforms/targets"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/passwordvault/api/platforms/targets"
+            }
+            write-verbose "MAKING API CALL"
+
+            if($sessionval){
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+            }
+            else{
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json"
+            }
+
+            $counter = $response.Total
+            Write-Verbose "FOUND $counter PLATFORMS...LOOKING FOR TARGET PLATFORMID: $searchQuery"
+
+            $output = -1
+            foreach($rec in $response.Platforms){
+                $recid = $rec.ID
+                $recplatformid = $rec.PlatformID
+                $recname = $rec.Name
+
+                if($recplatformid -eq $platformID -or $recname -eq $platformID){
+                    $output = [int]$recid
+                    Write-Verbose "FOUND $platformID : TARGET ENTRY FOUND, RETURNING ID"
+                    return $output
+                }
+                Write-Verbose "FOUND $recplatformid : NOT TARGET ENTRY (SKIPPING)"
+
+            }
+            Write-Verbose "CAN NOT FIND TARGET ENTRY, RETURNING -1"
+            return $output
+        }catch{
+            Write-Verbose "UNABLE TO QUERY CYBERARK"
+            Write-VPASOutput -str $_ -type E
+        }
+    }
+    End{
+
+    }
+}
---
+++ VpasModule/13.1.1/private/Get-VPASRecordingIDHelper.ps1
@@ -0,0 +1,94 @@
+<#
+.Synopsis
+   GET RECORDING ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   HELPER FUNCTION TO RETRIEVE RECORDING ID FROM CYBERARK
+#>
+function Get-VPASRecordingIDHelper{
+    [OutputType([String],'System.Int32',[bool])]
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$SearchQuery,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [Switch]$NoSSL
+
+    )
+
+    Begin{
+        $tokenval,$sessionval,$PVWA,$Header,$ISPSS,$IdentityURL = Get-VPASSession -token $token
+    }
+    Process{
+        try{
+            Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+            Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+            Write-Verbose "SUCCESSFULLY PARSED SEARCHQUERY VALUE: $SearchQuery"
+
+            if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/API/recordings?Search=$SearchQuery"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/API/recordings?Search=$SearchQuery"
+            }
+
+            write-verbose "MAKING API CALL TO CYBERARK"
+
+            if($sessionval){
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+            }
+            else{
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json"
+            }
+
+            $output = -1
+            foreach($rec in $response.Recordings){
+                $recSessionID = $rec.SessionID
+                $recUser = $rec.User
+                $recTargetAcct = $rec.AccountUsername
+                $recTargetAddr = $rec.AccountAddress
+
+                if($recSessionID -eq $SearchQuery -or $recUser -eq $SearchQuery -or $recTargetAcct -eq $SearchQuery -or $recTargetAddr -match $SearchQuery){
+                    write-verbose "FOUND TARGET RECORDING SESSION: $recSessionID...RETURNING RECORDING SESSION ID"
+                    if($output -eq -1){
+                        $output = $recSessionID
+                    }
+                    else{
+                        Write-Verbose "FOUND MULTIPLE TARGET ENTRIES, USE MORE SEARCH PARAMETES...RETURNING -2"
+                        Write-VPASOutput -str "FOUND MULTIPLE TARGET ENTRIES, USE MORE SEARCH PARAMETERS...RETURNING -2" -type E
+                        $output = -2
+                        return $output
+                    }
+                }
+                else{
+                    write-verbose "FOUND RECORDING SESSION: $recSessionID...NOT TARGET SESSION, SKIPPING"
+                }
+            }
+
+            if($output -ne -1){
+                Write-Verbose "FOUND MATCHING RECORIDNG SESSION ID...RETURNING RECORDING ID"
+                return $output
+            }
+            else{
+                Write-Verbose "CAN NOT FIND TARGET ENTRY, RETURNING -1"
+                Write-VPASOutput -str "CAN NOT FIND TARGET ENTRY, RETURNING -1" -type E
+                return $output
+            }
+
+        }catch{
+            Write-Verbose "UNABLE TO GET RECORDING SESSIONS FOR SEARCHQUERY: $SearchQuery"
+            Write-VPASOutput -str $_ -type E
+            return $false
+        }
+    }
+    End{
+
+    }
+}
