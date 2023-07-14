7663374499d52a6611c3679c472bbb3678ab31d9
vmelamed5 <vmelamed5@gmail.com>
2023-07-13 21:39:56 -0400

    Update Get-VPASSQLPlatforms.ps1

--- VpasModule/13.1.1/public/Get-VPASSQLPlatforms.ps1
+++ VpasModule/13.1.1/public/Get-VPASSQLPlatforms.ps1
@@ -47,13 +47,13 @@ function Get-VPASSQLPlatforms{
                 Write-Verbose "FOUND SQL CONFIG FILE...PARSING DATA"
             }
             else{
-                Write-Verbose "FAILED TO FIND SQL CONFIG FILE...RERUN VSetSQLConnectionDetails"
-                Write-VPASOutput -str "FAILED TO FIND SQL CONFIG FILE...RERUN VSetSQLConnectionDetails" -type E
+                Write-Verbose "FAILED TO FIND SQL CONFIG FILE...RERUN Set-VPASSQLConnectionDetails"
+                Write-VPASOutput -str "FAILED TO FIND SQL CONFIG FILE...RERUN Set-VPASSQLConnectionDetails" -type E
                 return $false
             }
         }catch{
-            Write-Verbose "FAILED TO FIND SQL CONFIG FILE...RERUN VSetSQLConnectionDetails"
-            Write-VPASOutput -str "FAILED TO FIND SQL CONFIG FILE...RERUN VSetSQLConnectionDetails" -type E
+            Write-Verbose "FAILED TO FIND SQL CONFIG FILE...RERUN Set-VPASSQLConnectionDetails"
+            Write-VPASOutput -str "FAILED TO FIND SQL CONFIG FILE...RERUN Set-VPASSQLConnectionDetails" -type E
             return $false
         }
 
@@ -228,7 +228,7 @@ function Get-VPASSQLPlatforms{
                 Write-VPASOutput -str $strsplit -type E
             }
 
-            Write-VPASOutput -str "DOWNLOAD THE MODULE BY TYPING IN 'Install-Module -Name SqlServer' THEN RERUN VSetSQLConnectionDetails" -type E
+            Write-VPASOutput -str "DOWNLOAD THE MODULE BY TYPING IN 'Install-Module -Name SqlServer' THEN RERUN Set-VPASSQLConnectionDetails" -type E
             Write-VPASOutput -str "YOU CAN ALSO VIEW THIS LINK FOR MORE INFORMATION: 'https://www.powershellgallery.com/packages/SqlServer/21.1.18256'" -type E
             Write-VPASOutput -str "PROCESS TERMINATED" -type E
             return $false
