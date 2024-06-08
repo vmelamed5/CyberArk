05edb8ae6e440377df56c5e0aef575b5e54eaa7d
vmelamed5 <vmelamed5@gmail.com>
2024-06-08 02:03:20 -0400

    Update Remove-VPASIdentityRole.ps1

--- VpasModule/14.1.0/public/Remove-VPASIdentityRole.ps1
+++ VpasModule/14.1.0/public/Remove-VPASIdentityRole.ps1
@@ -157,7 +157,7 @@ function Remove-VPASIdentityRole{
                     foreach($safe in $WhatIfAffectedSafes){
                         $miniHash = @{}
 
-                        $AffectedAccounts = Get-VPASAccountDetails -safe $safe -HideWarnings -token $token
+                        $AffectedAccounts = Get-VPASAccountDetails -safe $safe -HideWarning -token $token
 
                         foreach($AffectedAcct in $AffectedAccounts.value){
                             $AffectedAcctSafe = $AffectedAcct.safeName
