a491acdf227a1a849067fdfd3900c298442f8878
vmelamed5 <vmelamed5@gmail.com>
2024-07-15 23:28:50 -0500

    Add files via upload

---
+++ VpasModule/14.2.1/public/Invoke-VPASCredentialProvider.ps1
@@ -0,0 +1,74 @@
+<#
+.Synopsis
+   CREDENTIAL PROVIDER API CALL
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO RETRIEVE ACCOUNT INFORMATION VIA CREDENTIAL PROVIDER
+.PARAMETER ApplicationID
+   The application ID that has access to the safe that will retrieve the account information
+.PARAMETER Safe
+   Safe that the target account is located in
+.PARAMETER ObjectName
+   Unique identifier of the target account
+.PARAMETER Folder
+   A directory within a safe that the target account is located in
+   Default value: root
+.PARAMETER SDKLocation
+   Location or filepath to the CLIPasswordSDK that will be utilized to make the call
+   Default value: 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'
+.EXAMPLE
+   $CPResults = Invoke-VPASCredentialProvider -ApplicationID {APPLICATION ID VALUE} -Safe {SAFE VALUE} -ObjectName {OBJECT NAME VALUE} -Folder {FOLDER VALUE} -SDKLocation {SDKLOCATION VALUE}
+.OUTPUTS
+   JSON Object (CPResults) if successful
+   $false if failed
+#>
+function Invoke-VPASCredentialProvider{
+    [OutputType([bool])]
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="ApplicationID that has access to the safe that will retrieve the account information",Position=0)]
+        [String]$ApplicationID,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Safe that the target account is located in",Position=1)]
+        [String]$Safe,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Unique identifier of the target account",Position=2)]
+        [String]$ObjectName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,HelpMessage="A directory within a safe that the target account is located in (Default value: root)",Position=3)]
+        [String]$Folder,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Location or filepath to the CLIPasswordSDK that will be utilized to make the call (Default value: 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe')",Position=4)]
+        [String]$SDKLocation
+    )
+
+    Begin{
+
+    }
+    Process{
+        try{
+            if([String]::IsNullOrEmpty($Folder)){
+                Write-Verbose "NO FOLDER PASSED, USING DEFAULT VALUE: root"
+                $Folder = "root"
+            }
+            if([String]::IsNullOrEmpty($SDKLocation)){
+                Write-Verbose "NO SDKLocation PASSED, USING DEFAULT VALUE: 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'"
+                $SDKLocation = "C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe"
+            }
+
+            Write-Verbose "BUILDING SDK COMMAND"
+            Write-Verbose "$passwordSDK GetPassword /p AppDescs.AppID=$ApplicationID /p Query=`"Safe=$Safe;Folder=$Folder;Object=$ObjectName`" /o passprops.username"
+            $username = & $passwordSDK GetPassword /p AppDescs.AppID=$ApplicationID /p Query="Safe=$Safe;Folder=$Folder;Object=$ObjectName" /o passprops.username
+
+
+        }catch{
+            Write-Verbose "UNABLE TO RETRIEVE ACCOUNT DETAILS"
+            Write-Host $_ -ForegroundColor Red
+            return $false
+        }
+    }
+    End{
+
+    }
+}
