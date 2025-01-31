23b867af4348eefcfdae58777d75e0f70d6b6e64
vmelamed5 <vmelamed5@gmail.com>
2025-01-31 00:46:20 -0500

    Add files via upload

---
+++ VpasModule/12.6.1/private/VGetAccountGroupIDHelper.ps1
@@ -0,0 +1,70 @@
+<#
+.Synopsis
+   GET ACCOUNT GROUP ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   HELPER FUNCTION TO RETRIEVE ACCOUNT GROUP IDS FROM CYBERARK
+#>
+function VGetAccountGroupIDHelper{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$safe,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$GroupName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$NoSSL
+    
+    )
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+
+        Write-Verbose "RETRIEVING ALL GROUPS IN SAFE: $safe"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/API/AccountGroups?Safe=$safe"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/API/AccountGroups?Safe=$safe"
+        }
+        write-verbose "MAKING API CALL"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json"  
+        }
+
+        foreach($rec in $response){
+            $recgroupID = $rec.GroupID
+            $recgroupName = $rec.GroupName
+
+            if($recgroupName -eq $GroupName){
+                write-verbose "FOUND TARGET GROUPNAME: $GroupName...RETURNING GROUPID"
+                return $recgroupID
+            }
+            else{
+                write-verbose "FOUND GROUPNAME: $recgroupName...NOT TARGET GROUP, SKIPPING"
+            }
+        }
+
+        Write-Verbose "COULD NOT FIND TARGET GROUPID...RETURNING FALSE"
+        Vout -str "COULD NOT FIND TARGET GROUPID...RETURNING FALSE" -type E
+        return $false
+    }catch{
+        Write-Verbose "UNABLE TO QUERY CYBERARK"
+        Vout -str $_ -type E
+        return $false
+    }
+}
---
+++ VpasModule/12.6.1/private/VGetAuthenticationMethodIDHelper.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.Synopsis
+   GET AUTHENTICATION METHOD ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   HELPER FUNCTION TO RETRIEVE AUTHENTICATION METHOD IDS FROM CYBERARK
+#>
+function VGetAuthenticationMethodIDHelper{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$AuthenticationMethodSearch,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [Switch]$NoSSL   
+    )
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+
+        Write-Verbose "CONSTRUCTING SEARCH STRING TO QUERY CYBERARK"
+        $searchQuery = "$AuthenticationMethodSearch"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/passwordvault/api/Configuration/AuthenticationMethods/"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/passwordvault/api/Configuration/AuthenticationMethods/"
+        }
+        write-verbose "MAKING API CALL"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method GET -ContentType "application/json"  
+        }
+
+        $counter = $response.Methods.Count
+        Write-Verbose "FOUND $counter AUTHENTICATION METHODS...LOOKING FOR TARGET AUTHENTICATION METHOD: $searchQuery"
+
+        $output = -1
+        foreach($rec in $response.Methods){
+            $recid = $rec.id
+            $recdisplayname = $rec.displayName
+
+            if($recid -eq $AuthenticationMethodSearch -or $recdisplayname -eq $AuthenticationMethodSearch){
+                #$output = [int]$recid
+                $output = $recid
+                Write-Verbose "FOUND $AuthenticationMethodSearch : TARGET ENTRY FOUND, RETURNING AUTHENTICATION METHOD ID"
+                return $output
+            }
+            Write-Verbose "FOUND $recid : NOT TARGET ENTRY (SKIPPING)"
+
+        }
+        Write-Verbose "CAN NOT FIND TARGET ENTRY, RETURNING -1"
+        return $output
+    }catch{
+        Write-Verbose "UNABLE TO QUERY CYBERARK"
+        Vout -str $_ -type E
+    }
+}
