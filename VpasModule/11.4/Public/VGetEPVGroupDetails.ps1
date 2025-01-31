e46892e44a7870198b31f18d2bf64de4d838e167
vmelamed5 <vmelamed5@gmail.com>
2025-01-31 00:34:05 -0500

    Add files via upload

---
+++ VpasModule/11.4/Public/VGetEPVGroupDetails.ps1
@@ -0,0 +1,57 @@
+<#
+.Synopsis
+   GET EPV GROUP DETAILS
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO GET EPV GROUP(s) DETAILS
+.EXAMPLE
+   $EPVGroupDetailsJSON = VGetEPVGroupDetails -PVWA {PVWA VALUE} -token {TOKEN VALUE} -GroupName {GROUPNAME VALUE}
+.OUTPUTS
+   JSON Object (EPVGroupDetails) if successful
+   $false if failed
+#>
+function VGetEPVGroupDetails{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$PVWA,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$GroupName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED GROUPNAME VALUE: $GroupName"
+
+    try{
+        Write-Verbose "CONSTRUCTING SEARCH STRING TO QUERY CYBERARK"
+        $searchQuery = "$GroupName"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/api/UserGroups?search=$searchQuery"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/api/UserGroups?search=$searchQuery"
+        }
+
+        write-verbose "MAKING API CALL"
+        $response = Invoke-RestMethod -Headers @{"Authorization"=$token} -Uri $uri -Method GET
+
+        Write-Verbose "RETURNING JSON OBJECT"
+        return $response
+    }catch{
+        Write-Verbose "UNABLE TO FIND EPV GROUP: $UserName"
+        Vout -str $_ -type E
+        return $false
+    }
+}
