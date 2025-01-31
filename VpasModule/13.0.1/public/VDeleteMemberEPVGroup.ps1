d4cb69c04ea4d0a173c52df652456df9aa611d2d
vmelamed5 <vmelamed5@gmail.com>
2025-01-31 15:14:50 -0500

    Add files via upload

---
+++ VpasModule/13.0.1/public/VDeleteMemberEPVGroup.ps1
@@ -0,0 +1,93 @@
+<#
+.Synopsis
+   DELETE MEMBER FROM EPV GROUP
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO DELETE A MEMBER FROM AN EPV GROUP
+.EXAMPLE
+   $DeleteMemberEPVGroupStatus = VDeleteMemberEPVGroup -token {TOKEN VALUE} -GroupLookupBy GroupName -GroupLookupVal {GROUPNAME VALUE} -EPVUserName {USERNAME VALUE}
+.EXAMPLE
+   $DeleteMemberEPVGroupStatus = VDeleteMemberEPVGroup -token {TOKEN VALUE} -GroupLookupBy GroupID -GroupLookupVal {GROUPID VALUE} -EPVUserName {USERNAME VALUE}
+.OUTPUTS
+   $true if successful
+   $false if failed
+#>
+function VDeleteMemberEPVGroup{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [ValidateSet('GroupName','GroupID')]
+        [String]$GroupLookupBy,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$GroupLookupVal,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [String]$EPVUserName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [Switch]$NoSSL
+    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED GROUPLOOKUPBY VALUE: $GroupLookupBy"
+    Write-Verbose "SUCCESSFULLY PARSED GROUPLOOKUPVALUE VALUE: $GroupLookupVal"
+    Write-Verbose "SUCCESSFULLY PARSED EPVUSERNAME VALUE: $EPVUserName"
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+        $Header = $token.HeaderType
+        $ISPSS = $token.ISPSS
+
+        if($GroupLookupBy -eq "GroupName"){
+            Write-Verbose "CONSTRUCTING SEARCH STRING TO QUERY CYBERARK"
+            $searchQuery = "$GroupLookupVal"
+            Write-Verbose "INVOKING HELPER FUNCTION TO RETRIEVE GROUPID"
+
+            if($NoSSL){
+                $GroupID = VGetEPVGroupIDHelper -token $token -GroupName $GroupLookupVal -NoSSL
+                write-verbose "FOUND GROUPID: $GroupID"
+            }
+            else{
+                $GroupID = VGetEPVGroupIDHelper -token $token -GroupName $GroupLookupVal
+                write-verbose "FOUND GROUPID: $GroupID"
+            }
+        }
+        elseif($GroupLookupBy -eq "GroupID"){
+            Write-Verbose "SUPPLIED GROUPID, SKIPPING HELPER FUNCTION"
+            $GroupID = $GroupLookupVal
+        }
+
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/api/UserGroups/$GroupID/Members/$EPVUserName/"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/api/UserGroups/$GroupID/Members/$EPVUserName/"
+        }
+
+        Write-Verbose "MAKING API CALL TO CYBERARK"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method DELETE -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method DELETE -ContentType "application/json"  
+        }
+        Write-Verbose "SUCCESSFULLY DELETED $EPVUserName FROM $GroupLookupBy : $GroupLookupVal"
+        return $true
+        
+    }catch{
+        Write-Verbose "UNABLE TO DELETE $EPVUserName FROM $GroupLookupBy : $GroupLookupVal"
+        Vout -str $_ -type E
+        return $false
+    }
+}
