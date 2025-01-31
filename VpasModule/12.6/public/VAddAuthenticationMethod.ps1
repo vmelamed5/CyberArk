08ffb5dd1ede4b8f5e76920a8dddfaca33563350
vmelamed5 <vmelamed5@gmail.com>
2025-01-31 00:45:52 -0500

    Add files via upload

---
+++ VpasModule/12.6/public/VAddAuthenticationMethod.ps1
@@ -0,0 +1,156 @@
+<#
+.Synopsis
+   ADD AUTHENTICATION METHOD
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO ADD AUTHENTICATION METHOD INTO CYBERARK
+.EXAMPLE
+   $AddAuthenticationMethodJSON = VAddAuthenticationMethod -token {TOKEN VALUE} -AuthenticationMethodID (AUTHENTICATION METHOD IS VALUE}
+.OUTPUTS
+   JSON Object (AuthenticationMethod) if successful
+   $false if failed
+#>
+function VAddAuthenticationMethod{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$AuthenticationMethodID,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$DisplayName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [ValidateSet('TRUE','FALSE')]
+        [String]$Enabled,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [ValidateSet('TRUE','FALSE')]
+        [String]$MobileEnabled,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=5)]
+        [String]$LogoffURL,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=6)]
+        [ValidateSet('cyberark','radius','ldap')]
+        [String]$SecondFactorAuth,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=7)]
+        [String]$SignInLabel,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=8)]
+        [String]$UsernameFieldLabel,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=9)]
+        [String]$PasswordFieldLabel,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=10)]
+        [Switch]$NoSSL    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED AUTHENTICATION METHOD ID: $AuthenticationMethodID"
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+
+        Write-Verbose "INITIALIZING PARAMETERS"
+        $params = @{
+            id = $AuthenticationMethodID
+        }
+
+        if([String]::IsNullOrEmpty($DisplayName)){ Write-Verbose "NO DISPLAY NAME PASSED...SKIPPING" }
+        else{
+            $params += @{ displayName = $DisplayName }
+        }
+
+        if([String]::IsNullOrEmpty($LogoffURL)){ Write-Verbose "NO LOGOFF URL PASSED...SKIPPING" }
+        else{
+            $params += @{ logoffUrl = $LogoffURL }
+        }
+
+        if([String]::IsNullOrEmpty($SecondFactorAuth)){ Write-Verbose "NO SECOND FACTOR AUTH PASSED...SKIPPING" }
+        else{
+            $params += @{ secondFactorAuth = $SecondFactorAuth }
+        }
+
+        if([String]::IsNullOrEmpty($SignInLabel)){ Write-Verbose "NO SIGN IN LABEL PASSED...SKIPPING" }
+        else{
+            $params += @{ signInLabel = $SignInLabel }
+        }
+        
+        if([String]::IsNullOrEmpty($UsernameFieldLabel)){ Write-Verbose "NO USERNAME FIELD LABEL PASSED...SKIPPING" }
+        else{
+            $params += @{ usernameFieldLabel = $UsernameFieldLabel }
+        }
+
+        if([String]::IsNullOrEmpty($PasswordFieldLabel)){ Write-Verbose "NO PASSWORD FIELD LABEL PASSED...SKIPPING" }
+        else{
+            $params += @{ passwordFieldLabel = $PasswordFieldLabel }
+        }  
+           
+        if([String]::IsNullOrEmpty($Enabled)){
+            Write-Verbose "ENABLED FLAG NOT PASED...SETTING DEFAULT DISABLED"
+            $params += @{ enabled = $false }
+        }        
+        else{
+            if($Enabled -eq "TRUE"){
+                Write-Verbose "ENABLED FLAG SET TO TRUE"
+                $params += @{ enabled = $true }
+            }
+            else{
+                Write-Verbose "ENABLED FLAG SET TO FALSE"
+                $params += @{ enabled = $false }
+            }
+        }
+
+        if([String]::IsNullOrEmpty($MobileEnabled)){
+            Write-Verbose "MOBILE ENABLED FLAG NOT PASED...SETTING DEFAULT DISABLED"
+            $params += @{ mobileEnabled = $false }
+        }        
+        else{
+            if($MobileEnabled -eq "TRUE"){
+                Write-Verbose "MOBILE ENABLED FLAG SET TO TRUE"
+                $params += @{ mobileEnabled = $true }
+            }
+            else{
+                Write-Verbose "MOBILE ENABLED FLAG SET TO FALSE"
+                $params += @{ mobileEnabled = $false }
+            }
+        }
+
+        $params = $params | ConvertTo-Json
+        Write-Verbose "FINALIZING PARAMETERS"
+        
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/PasswordVault/API/Configuration/AuthenticationMethods/"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/PasswordVault/API/Configuration/AuthenticationMethods/"
+        }
+
+        write-verbose "MAKING API CALL TO CYBERARK"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Body $params -Method POST -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Body $params -Method POST -ContentType "application/json"  
+        }
+        Write-Verbose "SUCCESSFULLY ADDED AUTHENTICAT METHOD ID: $AuthenticationMethodID"
+        Write-Verbose "RETURNING JSON OBJECT"
+
+        return $response
+    }catch{
+        Write-Verbose "UNABLE TO ADD AUTHENTICATION METHOD ID"
+        Vout -str $_ -type E
+        return $false
+    }
+}
---
+++ VpasModule/12.6/public/VDeleteAuthenticationMethod.ps1
@@ -0,0 +1,76 @@
+<#
+.Synopsis
+   DELETE AUTHENTICATION METHOD
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   USE THIS FUNCTION TO DELETE AUTHENTICATION METHOD INTO CYBERARK
+.EXAMPLE
+   $DeleteAuthenticationMethodStatus = VDeleteAuthenticationMethod -token {TOKEN VALUE} -AuthMethodID {AUTH METHOD ID VALUE}
+.OUTPUTS
+   $true if successful
+   $false if failed
+#>
+function VDeleteAuthenticationMethod{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [String]$AuthMethodSearch,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$AuthMethodID,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [Switch]$NoSSL    
+    )
+
+    Write-Verbose "SUCCESSFULLY PARSED PVWA VALUE"
+    Write-Verbose "SUCCESSFULLY PARSED TOKEN VALUE"
+
+    try{
+        $tokenval = $token.token
+        $sessionval = $token.session
+        $PVWA = $token.pvwa
+
+        if([String]::IsNullOrEmpty($AuthMethodID)){
+            Write-Verbose "NO AUTH METHOD ID PROVIDED...INVOKING HELPER FUNCTION TO RETRIEVE UNIQUE AUTH METHOD ID BASED ON SPECIFIED PARAMETERS"
+            if($NoSSL){
+                $AuthMethodID = VGetAuthenticationMethodIDHelper -token $token -AuthenticationMethodSearch $AuthMethodSearch -NoSSL
+            }
+            else{
+                $AuthMethodID = VGetAuthenticationMethodIDHelper -token $token -AuthenticationMethodSearch $AuthMethodSearch
+            }
+            Write-Verbose "RETURNING AUTH METHOD ID"
+        }
+        else{
+            Write-Verbose "AUTH METHOD ID SUPPLIED, SKIPPING HELPER FUNCTION"
+        }
+
+        if($NoSSL){
+            Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+            $uri = "http://$PVWA/passwordvault/api/Configuration/AuthenticationMethods/$AuthMethodID/"
+        }
+        else{
+            Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+            $uri = "https://$PVWA/passwordvault/api/Configuration/AuthenticationMethods/$AuthMethodID/"
+        }
+        write-verbose "MAKING API CALL TO CYBERARK"
+
+        if($sessionval){
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method DELETE -ContentType "application/json" -WebSession $sessionval
+        }
+        else{
+            $response = Invoke-RestMethod -Headers @{"Authorization"=$tokenval} -Uri $uri -Method DELETE -ContentType "application/json"  
+        }
+        Write-Verbose "AUTHENTICATION METHOD IS HAS BEEN DELETED"
+        Write-Verbose "RETURNING TRUE"
+
+        return $true
+    }catch{
+        Write-Verbose "UNABLE TO DELETE AUTHENTICATION METHOD ID"
+        Vout -str $_ -type E
+        return $false
+    }
+}
