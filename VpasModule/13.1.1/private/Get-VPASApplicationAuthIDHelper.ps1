44f5bc35e2afb9b8ee5ac3caf67e812c1f143d57
vmelamed5 <vmelamed5@gmail.com>
2025-01-31 15:23:04 -0500

    Add files via upload

---
+++ VpasModule/13.1.1/private/Get-VPASApplicationAuthIDHelper.ps1
@@ -0,0 +1,80 @@
+<#
+.Synopsis
+   RETRIEVE AUTHID FOR APPLICATIONID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   THIS IS A HELPER FUNCTION TO RETRIEVE AUTHID OF AN APPLICATIONID IN CYBERARK
+#>
+function Get-VPASApplicationAuthIDHelper{
+    [OutputType([String],'System.Int32')]
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$AppID,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [ValidateSet('path','hash','osuser','machineaddress','certificateserialnumber')]
+        [String]$AuthType,
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [String]$AuthValue,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=3)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=4)]
+        [Switch]$NoSSL
+
+    )
+
+    Begin{
+        $tokenval,$sessionval,$PVWA,$Header,$ISPSS,$IdentityURL = Get-VPASSession -token $token
+    }
+    Process{
+        try{
+            Write-Verbose "HELPER FUNCTION INITIATED"
+
+            if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$PVWA/PasswordVault/WebServices/PIMServices.svc/Applications/$AppID/Authentications"
+            }
+
+            if($sessionval){
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json" -WebSession $sessionval
+            }
+            else{
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method GET -ContentType "application/json"
+            }
+
+            $outputflag = 0
+            $output = $response.authentication
+            foreach($res in $output){
+                if($res.AuthType -eq $AuthType -and $res.AuthValue -eq $AuthValue){
+                    $AuthID = $res.authID
+                    $outputflag = 1
+                }
+            }
+        }catch{
+            $outputflag = -1
+            Write-VPASOutput -str "COULD NOT FIND $AuthType || $AuthValue UNDER APPID: $AppID" -type E
+            Write-Verbose "COULD NOT FIND $AuthType || $AuthValue UNDER APPID: $AppID"
+        }
+
+        if($outputflag -eq 1){
+            Write-Verbose "RETURNING AUTHID VALUE"
+            return $AuthID
+        }
+        else{
+            Write-Verbose "COULD NOT FIND SPECIFIED AUTHENTICATION METHOD"
+            return -1
+        }
+    }
+    End{
+
+    }
+}
---
+++ VpasModule/13.1.1/private/Get-VPASSecurityQuestionIDIdentityHelper.ps1
@@ -0,0 +1,91 @@
+﻿<#
+.Synopsis
+   GET ADMIN SECURITY QUESTION ID
+   CREATED BY: Vadim Melamed, EMAIL: vmelamed5@gmail.com
+.DESCRIPTION
+   HELPER FUNCTION TO RETRIEVE ADMIN SECURITY QUESTION IDS FROM IDENTITY
+#>
+function Get-VPASSecurityQuestionIDIdentityHelper{
+    [OutputType([String],'System.Int32',[bool])]
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [String]$SecurityQuestion,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=1)]
+        [hashtable]$token,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=2)]
+        [Switch]$NoSSL
+
+    )
+
+    Begin{
+        $tokenval,$sessionval,$PVWA,$Header,$ISPSS,$IdentityURL = Get-VPASSession -token $token
+    }
+    Process{
+        try{
+            if(!$IdentityURL){
+                Write-VPASOutput -str "LOGIN TOKEN WAS NOT GENERATED THROUGH IDENTITY, TERMINATING API CALL" -type E
+                return $false
+            }
+
+            if($NoSSL){
+                Write-Verbose "NO SSL ENABLED, USING HTTP INSTEAD OF HTTPS"
+                $uri = "http://$IdentityURL/TenantConfig/GetAdminSecurityQuestions"
+            }
+            else{
+                Write-Verbose "SSL ENABLED BY DEFAULT, USING HTTPS"
+                $uri = "https://$IdentityURL/TenantConfig/GetAdminSecurityQuestions"
+            }
+            write-verbose "MAKING API CALL"
+
+            if($sessionval){
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method POST -ContentType "application/json" -WebSession $sessionval
+            }
+            else{
+                $response = Invoke-RestMethod -Headers @{"Authorization"=$Header} -Uri $uri -Method POST -ContentType "application/json"
+            }
+            $result = $response
+
+            $counter = 0
+            $returnID = ""
+            foreach($question in $result.Result){
+                $recQuestionID = $question.Uuid
+                $recQuestion = $question.Question
+
+                if($recQuestion -match $SecurityQuestion){
+                    $counter += 1
+                    $returnID = $recQuestionID
+                }
+
+                if($recQuestion -eq $SecurityQuestion){
+                    Write-Verbose "FOUND TARGET SECURITY QUESTION, RETURNING UNIQUE UUID"
+                    return $recQuestionID
+                }
+            }
+
+            if($counter -gt 1){
+                Write-Verbose "MULTIPLE SECURITY QUESTION ENTRIES WERE RETURNED, ADD MORE TO QUERY TO NARROW RESULTS"
+                return -1
+            }
+            elseif($counter -eq 0){
+                Write-Verbose "NO SECURITY QUESTIONS FOUND"
+                Write-VPASOutput -str "NO SECURITY QUESTIONS FOUND" -type E
+                return -2
+            }
+            else{
+                write-verbose "FOUND UNIQUE SECURITY QUESTION UUID"
+                Write-Verbose "RETURNING UNIQUE SECURITY QUESTION UUID"
+                return $returnID
+            }
+        }catch{
+            Write-Verbose "UNABLE TO QUERY IDENTITY"
+            Write-VPASOutput -str $_ -type E
+        }
+    }
+    End{
+
+    }
+}
